956f44c4da28e1903354e0db1893a58eee3acb1e
murilo-dev9 <murl09g@gmail.com>
2026-02-27 13:49:31 -0300

    aula 27/02

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,9 @@ export default Home;
   return (
     <div>
       <h1>react com next JS</h1>
+      <h2>aula intrdução</h2>
+      <h3>leiaute e estilização</h3>
+      <p>nesta aula iremos entender o uso de CSS global o local.</p>
       </div>
   );
 }
