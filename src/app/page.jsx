62b51644f5512eecc2c9068290c0f0151ddb92d2
murilo-dev9 <murl09g@gmail.com>
2026-03-13 11:46:01 -0300

    aula 13/03

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+export default Home;
 
 function Home() {
   return (
-    <div className={styles.containerExemplo}>
-      <h1>React com Next JS</h1>
-      <h2>Aula de introdução</h2>
-      <h3>Leiaute e Estilização</h3>
-      <p>Nesta aula iremos entender o uso de CSS global e local.</p>
-      <label>Exemplo Label</label>
-      <small>Exemplo Small</small>
-      <div className={styles.conteinerExPosicao}>
-        <div className={styles.divVermelha}></div>
-        <div className={styles.divVerde}></div>
-      </div>
+    <div className={styles.conteinerHome}>
+    <div className={styles.conteinerLista}>
+      <h1>Exemplos</h1>
+    </div>
+    <div className={styles.conteinerLista}>
+      <h1>Atividades</h1>
+    </div>
     </div>
   );
 }
 
-export default Home;
