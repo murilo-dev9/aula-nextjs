a41b533c86ad20677baf41464e8af7627a2889c4
murilo-dev9 <murl09g@gmail.com>
2026-03-16 15:23:23 -0300

    finalização atv1

--- src/app/atividades/atv-01/page.jsx
+++ src/app/atividades/atv-01/page.jsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
+import style from "./page.module.css";
 import Image from "next/image";
-import salsicha from '../public/file.svg';
+import salsicha from './img/cachorro.webp';
 
 export default atividade01;
 
 
 function atividade01(){
     return(
-        <div>
+        <div className={style.stlG}>
             <h1>atividade 1</h1>
-            <div>
+            <div className={style.stl1}>
                 <h1>apenas um cachorro salsicha</h1>
                 <Image src={salsicha}></Image>
                 <Link href="/">voltar</Link>
