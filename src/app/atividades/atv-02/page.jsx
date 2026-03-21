d233432934e904bb58573c916961c806608bc59b
murilo-dev9 <murl09g@gmail.com>
2026-03-21 18:08:19 -0300

    atividade 2 final

--- src/app/atividades/atv-02/page.jsx
+++ src/app/atividades/atv-02/page.jsx
@@ -1,16 +1,39 @@
+'use client'
+
+import { useState } from "react";
 import Link from "next/link";
 import style from "./page.module.css"
 
 export default function contador() {
-    return(
-    <div>
-        <h1>Atividade 2</h1>
-        <h2>contador para truco</h2>
-        <div>
-            <h1>time 1</h1>
-            <label></label>
-            <label onClick={dds}></label>
+
+    const [num1, setNum1] = useState(0);
+
+    function handleNum1() {
+        setNum1(num1 + 1);
+    }
+
+    const [num2, setNum2] = useState(0);
+
+    function handleNum2() {
+        setNum2(num2 + 1);
+    }
+
+    return (
+        <div className={style.gr}>
+            <h1>Atividade 2</h1>
+            <h2>contador para truco</h2>
+            <div className={style.pn}>
+                <div className={style.ctd}>
+                    <h1>time 1</h1>
+                    <label>{`${num1} pontos:`}</label>
+                    <label onClick={handleNum1}>+1</label>
+                </div>
+                <div className={style.ctd}>
+                    <h1>time 2</h1>
+                    <label>{`${num2} pontos:`}</label>
+                    <label onClick={handleNum2}>+1</label>
+                </div>
+            </div>
         </div>
-    </div>
     )
 }
