616c64b41afb652f2f9dd5c8fb95deaf887a04ec
murilo-dev9 <murl09g@gmail.com>
2026-03-27 11:44:00 -0300

    atividade 3 inicio

---
+++ src/app/atividades/atv-03/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Style from"./page.module.css"
+
+export default function Componentes (){
+    return(
+        <div>
+            <h1>Atividade 3</h1>
+            <label >`Ação selecionada`</label>
+        </div>
+    )
+}
