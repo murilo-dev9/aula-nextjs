f2ee45a23d4a229c9e4fd84fc7a29d23d6a698e3
murilo-dev9 <murl09g@gmail.com>
2026-04-06 16:59:53 -0300

    atividade 3 completa

--- src/app/atividades/atv-03/botão/index.jsx
+++ src/app/atividades/atv-03/botão/index.jsx
@@ -3,7 +3,7 @@ import style from "./index.module.css"
 function Botao({text, click, action}){
     return(
         
-        <button onClick={click} className={`${style.bt} ${action === ""? style.c1 :action==="caim"?style.c2:action==="miguel"?style.c3:action==="Jesus"?style.c4:action==="lucifer"?style.c5:style.bt}`}>
+        <button onClick={click} className={`${style.bt} ${action === "c1"? style.c1 :action==="c2"?style.c2:action==="c3"?style.c3:action==="c4"?style.c4:action==="c5"?style.c5:style.bt}`}>
             {text}
         </button>
 
--- src/app/atividades/atv-03/page.jsx
+++ src/app/atividades/atv-03/page.jsx
@@ -3,42 +3,44 @@
 import { useState } from "react";
 import Botao from "./botão";
 import Link from "next/link";
-import Style from"./page.module.css"
+import Style from "./page.module.css";
 
-export default function Componentes (){
-    
- const [name, setName] = useState("");
+export default function Componentes() {
 
-    function handleabel(){
+    const [name, setName] = useState("");
+
+    function handleabel() {
         setName("Abel");
     }
-    
-    function handlemiguel(){
+
+    function handlemiguel() {
         setName("Miguel");
     }
 
-    function handlelucifer(){
+    function handlelucifer() {
         setName("Lucifer");
     }
 
-    function handlecaim(){
+    function handlecaim() {
         setName("Caim");
     }
 
-    function handleJesus(){
+    function handleJesus() {
         setName("Jesus");
     }
-    return(
-        <div>
+    return (
+        <div className={Style.gr}>
             <h1>Atividade 3</h1>
             <label>{`nome ${name} `}</label>
             <div className={Style.bt}>
-            <Botao text={"Jesus"} click={handleJesus} action={""}></Botao>
-            <Botao text={"Abel"} click={handleabel} action={"Abel"}></Botao>
-            <Botao text={"miguel"} click={handlemiguel} action={"miguel"}></Botao>
-            <Botao text={"caim"} click={handlecaim} action={"caim"}></Botao>
-            <Botao text={"lucifer"} click={handlelucifer} action={"lucifer"}></Botao>
+                <Botao text={"Jesus"} click={handleJesus} action={"c1"}></Botao>
+                <Botao text={"Abel"} click={handleabel} action={"c2"}></Botao>
+                <Botao text={"miguel"} click={handlemiguel} action={"c3"}></Botao>
+                <Botao text={"caim"} click={handlecaim} action={"c4"}></Botao>
+                <Botao text={"lucifer"} click={handlelucifer} action={"c5"}></Botao>
+            </div>
+            <Link href={"/"}>voltar</Link>
         </div>
-        </div>
+        
     )
 }
