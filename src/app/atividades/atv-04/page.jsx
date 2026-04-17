4e3d9c8560154ae404dcb47b76779d2ec4d2bf15
murilo-dev9 <murl09g@gmail.com>
2026-04-17 11:44:35 -0300

    atividade 4 inicio

--- src/app/atividades/atv-04/page.jsx
+++ src/app/atividades/atv-04/page.jsx
@@ -1,1 +1,5 @@
-import style from "./page.module.css"
+import style from "./page.module.css";
+
+export default function(){
+    
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,12 +10,15 @@ function Home() {
       <Link href="/exemplos/ex-01">exemplo 1</Link>
       <Link href="/exemplos/ex-02">exemplo 2</Link>
       <Link href="/exemplos/ex-03">exemplo 3</Link>
+      <Link href="/exemplos/ex-04">exemplo 4</Link>
+      <Link href="/exemplos/ex-05">exemplo 5</Link>
     </div>
     <div className={styles.conteinerLista}>
       <h1>Atividades</h1>
       <Link href="/atividades/atv-01">atividade1</Link>
       <Link href="/atividades/atv-02">atividade2</Link>
       <Link href="/atividades/atv-03">atividade3</Link>
+      <Link href="/atividades/atv-04">atividade4</Link>
       <Link href="/atividades/atv-04(Professor)">atividade4(Professor)</Link>
     </div>
     </div>
