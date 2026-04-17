8cedfc0e9d375dd8012cdfa603f135a0bcaf5f49
MURILO DA SILVA LIMA <murilo.lima81>
2026-04-17 13:48:50 -0300

    atividade 4 botão excluir

--- src/app/atividades/atv-04/page.jsx
+++ src/app/atividades/atv-04/page.jsx
@@ -1,5 +1,40 @@
+'use client'
+import {use, useState} from 'react'
 import style from "./page.module.css";
 
-export default function(){
-    
+export default function formComponent (){
+    const[inputValue, setInputValue] = useState({
+        id:'', quant:'', nome:'',
+    });
+   
+
+    const [historico, setHistorico] = useState([])
+
+    const handleSubmit = (e) => 
+        { e.preventDfault()
+            if (!inputValue.produto || !inputValue.quant) return
+                const novaEntrada = `${quant} x ${name}`
+                setHistorico([...novaEntrada])
+            
+    }
+
+return(
+    <div>
+        <form onSubmit={handleSubmit} className={style.form}>
+            <h2>Lista de produtos</h2>
+            <input type="number" placeholder='quantidade' onChange={e => setInputValue({...inputValue, quant:e.target.value})}/>
+            <input type="text" placeholder='nome do produto' onChange={e => setInputValue({...inputValue, nome:e.target.value})}/>
+            <button type="submit">enviar</button>
+        </form>
+        <div>
+            {
+                historico.map(item => <p key={item}>{item}</p>)
+            }
+        </div>
+
+    </div>
+)
+
+
+
 }
